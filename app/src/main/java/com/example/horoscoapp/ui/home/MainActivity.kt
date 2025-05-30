5fca2aa937a8224500dd45ad1b3411a798a216ea
chrisgead <chinucha.pro@gmail.com>
2025-05-30 15:49:00 -0500

    add toolbar

--- app/src/main/java/com/example/horoscoapp/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/horoscoapp/ui/home/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.example.horoscoapp.ui.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.WindowInsetsController
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
--- app/src/main/java/com/example/horoscoapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/example/horoscoapp/ui/luck/LuckFragment.kt
@@ -1,12 +1,10 @@
 package com.example.horoscoapp.ui.luck
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.horoscoapp.R
-import com.example.horoscoapp.databinding.FragmentHoroscopeBinding
+import androidx.fragment.app.Fragment
 import com.example.horoscoapp.databinding.FragmentLuckBinding
 
 class LuckFragment : Fragment() {
