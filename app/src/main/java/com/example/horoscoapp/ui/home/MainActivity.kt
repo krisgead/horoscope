2181298f10927b12ece26654d63b02a64c5f084e
chrisgead <chinucha.pro@gmail.com>
2025-05-30 09:13:29 -0500

    Initial commit

---
+++ app/src/main/java/com/example/horoscoapp/ui/home/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.horoscoapp.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.WindowInsetsController
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.horoscoapp.R
+import com.example.horoscoapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI(){
+        initNavigation()
+    }
+
+    private fun initNavigation(){
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/horoscoapp/ui/palmistry/PalmistryFragment.kt
@@ -0,0 +1,27 @@
+package com.example.horoscoapp.ui.palmistry
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.horoscoapp.R
+import com.example.horoscoapp.databinding.FragmentLuckBinding
+import com.example.horoscoapp.databinding.FragmentPalmistryBinding
+
+
+class PalmistryFragment : Fragment() {
+
+    private var _binding: FragmentPalmistryBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPalmistryBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+}
